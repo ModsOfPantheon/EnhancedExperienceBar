a0cadeffa8561a15269384435e96d77ab5eba0cb
rkam <rkam@users.noreply.github.com>
2025-04-01 23:52:06 -0700

    Add experience gained

--- EnhancedExperienceBar/BarEnhancer.cs
+++ EnhancedExperienceBar/BarEnhancer.cs
@@ -8,11 +8,17 @@ namespace EnhancedExperienceBar;
 public static class BarEnhancer
 {
     private static TextMeshProUGUI? _text;
-    
+
+    private static double _lastExp;
+    private static double _lastNext;
+
     public static void OnLocalPlayerReady(EntityPlayerGameObject entityPlayerGameObject)
     {
         var exp = entityPlayerGameObject.Experience;
-        
+
+        _lastNext = exp.CalculateExperienceRequiredToNextLevel();
+        _lastExp = exp.CalculateCurrentExperienceIntoLevel();
+
         OnExperienceChanged(new PlayerExperience(exp.Level, exp.CalculateCurrentExperienceIntoLevel(), exp.CalculateExperienceRequiredToNextLevel(), exp.CalculatePercentThroughCurrentLevel()));
     }
 
@@ -20,15 +26,15 @@ public static class BarEnhancer
     {
         var xpWindowRect = xpWindow.GetComponent<RectTransform>();
         xpWindowRect.sizeDelta = new Vector2(xpWindowRect.sizeDelta.x, xpWindowRect.sizeDelta.y + 10);
-        
+
         var textGo = new GameObject("Test");
         textGo.transform.SetParent(xpWindow.transform);
-        
+
         var textComponent = textGo.AddComponent<TextMeshProUGUI>();
         textComponent.text = "0 / 0";
         textComponent.fontSize = 16;
         textComponent.alignment = TextAlignmentOptions.Center;
-        
+
         var textRect = textGo.GetComponent<RectTransform>();
         textRect.sizeDelta = new Vector2(500, 20);
         textRect.anchoredPosition = new Vector2(0, 0);
@@ -42,12 +48,26 @@ public static class BarEnhancer
         {
             return;
         }
-        
-        _text.text = CreateText(playerExperience);
+
+        double diff;
+        if (playerExperience.ToNextLevel == _lastNext)
+        {
+          diff = playerExperience.Current - _lastExp;
+        } else {
+          // Each level, Current starts at 0
+
+          diff = _lastNext - _lastExp + playerExperience.Current;
+          _lastNext = playerExperience.ToNextLevel;
+          _lastExp = 0;
+        }
+
+        _lastExp = playerExperience.Current;
+
+        _text.text = CreateText(playerExperience, diff);
     }
-    
-    private static string CreateText(PlayerExperience playerExperience)
+
+    private static string CreateText(PlayerExperience playerExperience, double diff)
     {
-        return $"{playerExperience.Current:N0} / {playerExperience.ToNextLevel:N0} ({playerExperience.ExperiencePercentage * 100:F}%)";
+        return $"{playerExperience.Current:N0} / {playerExperience.ToNextLevel:N0} ({playerExperience.ExperiencePercentage * 100:F}% +{diff})";
     }
 }
